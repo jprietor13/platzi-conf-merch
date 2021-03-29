db22b716d8dc6546623d43c212f55582e1b03b6b
Juan Prieto <gadaf.78@hotmail.com>
2021-03-29 10:53:33 -0500

    feat: Layout and styles components Payment and Success

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -3,7 +3,17 @@ import '../assets/styles/containers/Payment.css';
 
 const Payment = () => {
   return (
-    <h1>Payment</h1>
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resumen del pedido:</h3>
+        <div className="Payment-button">
+          Boton de pago con Paypal
+        </div>
+      </div>
+      <div>
+        Sidebar
+      </div>
+    </div>
   );
 };
 
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -3,7 +3,15 @@ import '../assets/styles/containers/Success.css';
 
 const Success = () => {
   return (
-    <h1>Success</h1>
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Juan, Gracias por tu compra</h2>
+        <span>Tu pedido llegara en 3 dias a tu direccin</span>
+        <div className="Success-map">
+          Google maps
+        </div>
+      </div>
+    </div>
   );
 };
 
